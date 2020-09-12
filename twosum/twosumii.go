0c2225dad29ccf9e9bf87c72c7409d1f25dc653b
mckinnonn <mckinnonr03@gmail.com>
2020-09-12 16:47:41 -0400

    fix typo

--- twosum/twosumii.go
+++ twosum/twosumii.go
@@ -1,6 +1,6 @@
 package twosum
 
-// TSTwoPointer solves two sum but it is quicker than the hashmap method due to the input slice being sort (numbers is sorted)
+// TSTwoPointer solves two sum but it is quicker than the hashmap method due to the input slice being sorted (numbers is sorted)
 func TSTwoPointer(numbers []int, target int) []int {
 	left := 0
 	right := len(numbers) - 1
