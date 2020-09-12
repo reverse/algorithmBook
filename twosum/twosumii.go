b1c08ef16442e22d988a977e82b6cf0b50626c63
mckinnonn <mckinnonr03@gmail.com>
2020-09-12 16:45:17 -0400

    add twosum soln with sorted input array

---
+++ twosum/twosumii.go
@@ -0,0 +1,19 @@
+package twosum
+
+// TSTwoPointer solves two sum but it is quicker than the hashmap method due to the input slice being sort (numbers is sorted)
+func TSTwoPointer(numbers []int, target int) []int {
+	left := 0
+	right := len(numbers) - 1
+
+	for left < right {
+		complement := numbers[left] + numbers[right]
+		if complement > target {
+			right--
+		} else if complement < target {
+			left++
+		} else {
+			return []int{left + 1, right + 1}
+		}
+	}
+	return []int{}
+}
