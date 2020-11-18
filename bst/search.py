cc43b7a3f4a59dfa45806bd6e0b1cbf473593f3a
mckinnonn <mckinnonr03@gmail.com>
2020-11-18 13:23:03 -0500

    add binary Search algo

---
+++ bst/search.py
@@ -0,0 +1,17 @@
+class BST:
+    def __init__(self, value=0, left=None, right=None):
+         self.value = value
+         self.left = left
+         self.right = right
+    
+
+def SearchBSTIterative(tree, val):
+    while tree is not None:
+        if val < tree.val:
+            tree = tree.left
+        elif val > tree.val:
+            tree = tree.right
+        else:
+            return tree
+    
+    return tree
