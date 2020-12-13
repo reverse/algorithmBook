bd2557a5f85535767f748307d71b29ee2f70d929
mckinnonn <mckinnonr03@gmail.com>
2020-12-13 16:33:24 -0500

    add linked List reversal

---
+++ linkedList/ll.go
@@ -0,0 +1,19 @@
+package linkedList 
+
+type LinkedList struct {
+    Val int 
+    Next *LinkedList 
+}
+
+func Reverse(head *LinkedList) *LinkedList {
+    current := head 
+    var prev *LinkedList 
+
+    for current != nil {
+        next := current.Next // init our next variable so we can store it while we switch
+        current.Next = prev // reverse the next pointer to point to our previous (reversing)
+        prev = current // move our previous to what we just switched
+        current = next // move forward, give us a new node in the LL
+    }
+    return prev 
+}
